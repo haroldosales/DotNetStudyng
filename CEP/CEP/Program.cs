81d0296ef98f0b7db5571d387530454ac924d575
haroldosales <haroldognu@gmail.com>
2021-12-22 20:19:37 -0300

    up

---
+++ CEP/CEP/Program.cs
@@ -0,0 +1,18 @@
+﻿using CEP;
+using System.Text.Json;
+
+string cep = "";
+cep = Console.ReadLine();
+
+using (var client = new HttpClient())
+{
+    client.BaseAddress = new Uri("https://viacep.com.br/");
+
+    HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, String.Format("ws/{0}/json/",cep));
+
+    var response = client.Send(request);
+
+    var end = JsonSerializer.Deserialize<Endereco>(response.Content.ReadAsStringAsync().Result);
+
+    Console.WriteLine(end.bairro + ", "+end.localidade);
+}
