6c2fd7be693d55596307a89aa8884e6fde0e3ff4
haroldosales <haroldognu@gmail.com>
2022-06-19 21:35:13 -0300

    update

---
+++ CEP/ExercicioMedio/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Exericios Basico de Media
+        Console.WriteLine("Calculo de Pitagoras");
+
+        Console.WriteLine("Digite a altura");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine("Digite a base");
+        double body = Convert.ToDouble(Console.ReadLine());
+
+        double valorTotal = (body*2)+ (altura*2) / 2;
+
+        Console.WriteLine(valorTotal);
+    }
+}
+
